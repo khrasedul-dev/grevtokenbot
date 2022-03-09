c5a0f123fa7b995ec3256f8df54ba4defac2be15
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-03-09 13:52:43 +0600

    Update startMenu.js

--- modules/startMenu.js
+++ modules/startMenu.js
@@ -2,7 +2,7 @@ const startMenu = (ctx,data)=>{
     ctx.telegram.sendMessage(ctx.chat.id , `Hello ${ctx.from.first_name} \nWellcome to ${ctx.botInfo.username} \n\nAccount information: \nUserId: ${data[0].userId} \nName: ${data[0].name}` , {
         reply_markup:{
             keyboard: [
-                [{text: "Claim Rewards"},{text: "Withdraw"}],
+                [{text: "Withdraw"}],
                 [{text: "My account"},{text: "Help"}]
             ],
             resize_keyboard: true
